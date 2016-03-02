69551f27a59b03403ff0d6e8fdaf1f556d12d05b
Mir Nawaz <you@example.com>
2016-03-02 11:58:38 +0500

    facade :dizzy:

---
+++ crust/controllers/users.js
@@ -0,0 +1,63 @@
+
+var UserFacade = require('../../lib/facade/users');
+
+module.exports = {
+  getAll,
+  save,
+  getById,
+  deleteById,
+  updateById
+};
+
+function getAll(req, res, next){
+    var userFacade = new UserFacade(req);
+    userFacade.getAll(req.getInputObject()).then(function(output){
+        res.status(200).send(global.shape(output))
+    }).catch(function(e){
+        next(e);
+    });
+}
+
+function save(req, res, next){
+
+    var userFacade = new UserFacade(req);
+
+    userFacade.save(req.getInputObject()).then(function(output){
+        res.status(200).send(global.shape(output))
+    }).catch(function(e){
+            next(e);
+        });
+}
+
+function getById(req, res, next){
+
+    var userFacade = new UserFacade(req);
+
+    userFacade.getById(req.getInputObject()).then(function(output){
+        res.status(200).send(global.shape(output))
+    }).catch(function(e){
+            next(e);
+        });
+}
+
+function deleteById(req, res, next){
+
+    var userFacade = new UserFacade(req);
+
+    userFacade.deleteById(req.getInputObject()).then(function(output){
+        res.status(200).send(global.shape(output))
+    }).catch(function(e){
+            next(e);
+        });
+}
+
+function updateById(req, res, next){
+
+    var userFacade = new UserFacade(req);
+
+    userFacade.updateById(req.getInputObject()).then(function(output){
+        res.status(200).send(global.shape(output))
+    }).catch(function(e){
+            next(e);
+        });
+}
--- crust/routes/v1/users.js
+++ crust/routes/v1/users.js
@@ -1,11 +1,10 @@
-var testController = require("../../controllers/index");
-var Users = require('../../../lib/facade/users');
+var userController = require("../../controllers/users");
 var router = require('express').Router();
 
-router.get("/", Users.getAll)
-		.post("/", Users.save);
-router.get("/:id", Users.getById)
-		.delete("/:id", Users.deleteById)
-		.put("/:id", Users.updateById);
+router.get("/", userController.getAll)
+		.post("/", userController.save);
+router.get("/:id", userController.getById)
+		.delete("/:id", userController.deleteById)
+		.put("/:id", userController.updateById);
 
 module.exports = router;
