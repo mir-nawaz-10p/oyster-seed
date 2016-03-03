34ffa00bb21c45c98b032c1a3ce8a90ef821e3a0
Mir Nawaz <you@example.com>
2016-03-03 10:53:43 +0500

    validations for user :bowtie:

--- crust/appRoutes.js
+++ crust/appRoutes.js
@@ -1,8 +1,6 @@
 var routes = require("./routes/v1/index")();
 function appRoutes(app){
 
-    app.get("/", routes.main);
-
     app.use("/api/v1/users", routes.users);
 
 }
--- crust/routes/v1/main.js
+++
@@ -1,6 +0,0 @@
-var router = require('express').Router();
-router.get("/", function(req, res) {
-	res.send({hello: "World!"});
-});
-
-module.exports = router;
